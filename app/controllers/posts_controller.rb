072a026bd2a189c3f44bad5fe53d07a50e12b062
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-05-29 20:04:58 -0400

    post it..

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @post = Post.new(article_params)
+
+    @post.save
+    redirect_to @post
+  end
+
+
+  private
+    def article_params
+      params.require(:post).permit(:title, :text)
+    end
 end
