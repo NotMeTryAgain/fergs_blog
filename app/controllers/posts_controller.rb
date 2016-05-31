48637ddc9b94618ce21cc5ea12b43e5fd0085224
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-05-30 21:13:08 -0400

    callouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
     end
 
     def authorize_user
-      if !user_signed_in? || !current_user.admin?
+      if !current_user.admin?
         raise ActionController::RoutingError.new("Not Found")
       end
     end
